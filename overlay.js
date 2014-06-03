b202617587c18d2930cd0fad12383cd57531d7ab
imryaaan <ryan.tyler99@gmail.com>
2014-06-03 09:53:26 +0100

    new

---
+++ overlay.js
@@ -0,0 +1,236 @@
+ $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg').click(function(){
+        $('.overlay-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content').click(function(){
+        return false;
+    });
+ 
+});
+
+ $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup2').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg2').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content2').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg2').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg2').click(function(){
+        $('.overlay-bg2').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content2').click(function(){
+        return false;
+    });
+ 
+});
+
+  $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup3').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg3').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content3').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg3').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg3').click(function(){
+        $('.overlay-bg3').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content3').click(function(){
+        return false;
+    });
+ 
+});
+
+   $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup4').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg4').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content4').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg4').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg4').click(function(){
+        $('.overlay-bg4').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content4').click(function(){
+        return false;
+    });
+
+    });
+ 
+    $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup5').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg5').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content5').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg5').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg5').click(function(){
+        $('.overlay-bg5').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content5').click(function(){
+        return false;
+    });
+
+    });
+
+       $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup6').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg6').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content6').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg6').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg6').click(function(){
+        $('.overlay-bg6').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content6').click(function(){
+        return false;
+    });
+
+    });
+ 
+        $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup7').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg7').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content7').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg7').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg7').click(function(){
+        $('.overlay-bg7').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content7').click(function(){
+        return false;
+    });
+
+    });
+
+     $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup8').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg8').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content8').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg8').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg8').click(function(){
+        $('.overlay-bg8').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content8').click(function(){
+        return false;
+    });
+
+    });
+ 
+
+     $(document).ready(function(){
+    // show popup when you click on the link
+    $('.show-popup9').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        var docHeight = $(document).height(); //grab the height of the page
+        var scrollTop = $(window).scrollTop(); //grab the px value from the top of the page to where you're scrolling      
+        $('.overlay-bg9').show().css({'height' : docHeight}); //display your popup and set height to the page height
+        $('.overlay-content9').css({'top': scrollTop+20+'px'}); //set the content 20px from the window top  
+    });
+ 
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.overlay-bg9').hide(); // hide the overlay
+    });
+ 
+    // hides the popup if user clicks anywhere outside the container
+    $('.overlay-bg9').click(function(){
+        $('.overlay-bg9').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.overlay-content9').click(function(){
+        return false;
+    });
+
+    });
+ 
+
